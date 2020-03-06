8060736a7e64d4d247c35dda6d06a8f42d6ed736
sosa edison <sosa_edison@hotmail.com>
2020-03-06 17:55:59 -0600

    adding menu bar to dasboard page

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-import TicketContainer from './TicketContainer'
+//import TicketContainer from './TicketContainer'
+import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
 
+
+const useStyles = makeStyles( theme => ({
+    title: {
+        flexGrow: 1,
+        position: "relative",
+    }
+}))
 export default function Dashboard() {
+    const classes = useStyles();
     return (
-        <div className='dashboard-container'>
-            <TicketContainer />
-        </div>
+        <AppBar position="sticky">
+        <Toolbar>
+         <Typography variant="h6" className={classes.title} color="inherit" aria-label="menu">
+            About
+         </Typography>
+         <Typography variant="h6" className={classes.title} color="inherit" aria-label="menu" edge="start">
+            Contact
+         </Typography>
+        </Toolbar>
+      </AppBar>
     )
 }
