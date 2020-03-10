6f31dce48bb3e51e01aaa883ecfbbe1e6129e0d2
Lucy Post <lucy.post@live.com>
2020-03-10 11:10:44 -0500

    added login and sticky footer
    login is not functioning

--- src/App.js
+++ src/App.js
@@ -12,20 +12,25 @@ import Menus from './Components/Menus'
 import Dashboard from './Components/Dashboard'
 import AddMenu from './Components/AddMenu'
 import SignUp from './Components/SignUp'
+import StickyFooter from './Components/StickyFooter';
 //import ErrorPage from './Components/Errorpage'
 
 export default function App() {
   return (
-    <Router>
-     <Switch> 
-      <Route path ='/' component={Landing} exact />
-      <Route path ='/login' component={Login} />
-      <Route path ='/home' component={Home} />
-      <Route path ='/menus' component={Menus} />
-      <Route path ='/dashboard' component={Dashboard} />
-      <Route path ='/addMenu' component={AddMenu} />
-      <Route path ='/signup' component={SignUp} />
-     </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Switch> 
+          <Route path ='/' component={Landing} exact />
+          <Route path ='/login' component={Login} />
+          <Route path ='/home' component={Home} />
+          <Route path ='/menus' component={Menus} />
+          <Route path ='/dashboard' component={Dashboard} />
+          <Route path ='/addMenu' component={AddMenu} />
+          <Route path ='/signup' component={SignUp} />
+        </Switch>
+      </Router>
+      <StickyFooter></StickyFooter>
+    </div>
+    
   )
 }
