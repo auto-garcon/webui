a1961ef5e4b92e5ba0a6238b34673502f5643caa
Lucy Post <lucy.post@live.com>
2020-05-14 14:38:51 -0500

    more create menu

--- src/Components/AddMenuItemForm.jsx
+++ src/Components/AddMenuItemForm.jsx
@@ -2,12 +2,15 @@
     AddMenuItemForm Component: This component defines the structure of the menuItem and its inputs.
 */
 import React from 'react';
+import Checkbox from "./Checkbox";
+
 
  class AddMenuItemForm extends React.Component {
 
      constructor () {
          super();
          this.createMenuItem = this.createMenuItem.bind(this);
+         this.onCheck = this.onCheck.bind(this);
      }
 
      /*
@@ -18,31 +21,41 @@ import React from 'react';
      createMenuItem (event) {
          event.preventDefault();
 
+        console.log(this.props);
+
          //TODO: add allergen and image
          //NOTE: Took category option out
          const menuItem = {
-             itemName: this.itemName.value,
-             description: this.description.value,
-             idx: this.props.idx,
-             price: this.price.value,
-             image: this.image.value,
-             calories: this.calories.value,
+            itemName: this.itemName.value,
+            description: this.description.value,
+            idx: this.props.idx,
+            price: this.price.value,
+            image: this.image.value,
+            calories: this.calories.value,
          }
+
+         /*
+            meat: this.meat.value,
+            dairy: this.dairy.value,
+            nuts: this.nuts.value,
+            gluten: this.gluten.value,
+            soy: this.soy.value,
+            other: this.other.value,
+         */
 
          this.props.addMenuItem(menuItem);
          //reset the form so that the next new item can be created
          this.menuItemForm.reset();
      }
 
+     onCheck(event) {
+         this[event.target.name] = this[event.target.checked];
+     }
+
      //TODO: create file uploader and handle input, checkbox list for allergens
      //this method renders the menuItem builder 
      render () {
-        const allergens = ["MEAT", "DAIRY", "NUTS", "GLUTEN", "SOY", "OTHER"].map((allergen, key) =>
-            <div>
-                <input type="checkbox" id={allergen} name={allergen} value={allergen} />
-                <label for={allergen}>{allergen}</label>
-            </div>
-        );
+        const allergens = ["meat", "dairy", "nuts", "gluten", "soy", "other"];
         //menuItem structure for creating a new menuItem
         return (
              <form className="menuItem-edit" onSubmit={this.createMenuItem}
@@ -54,8 +67,20 @@ import React from 'react';
                 <input type="text" placeholder="Calories"
                     ref={(input) => { this.calories = input}} />
                 <textarea placeholder="Description"
-                    ref={(input) => { this.description= input}} />
-                {/*ADD ALLERGENS HERE */}
+                    ref={(input) => { this.description= input }} />
+                <div className="allergens">
+                    {allergens.map((allergen) => {
+                        return(
+                            <label>{allergen}: 
+                                <input
+                                    type="checkbox"
+                                    name={allergen}
+                                    checked={this[allergen]}
+                                />
+                            </label>
+                        );
+                    })}
+                </div>
                 {/*ADD FILE UPLOADER HERE */}
                 <input type='file' name='uploaded_file' accept='.png' 
                     ref={(input) => {this.image = input}}/>
