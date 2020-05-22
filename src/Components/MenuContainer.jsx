40acb1eacc90d0bf7649fd643bfd1982afbe605c
Lucy Post <lucy.post@live.com>
2020-05-22 09:45:52 -0500

    added back menu status

--- src/Components/MenuContainer.jsx
+++ src/Components/MenuContainer.jsx
@@ -65,11 +65,9 @@ export default function MenuContainer(props) {
                             {/*<Card.Img variant="top" src="holder.js/100px180?text=Image cap" />*/}
                             <Card.Body className="card-body">
                                 <Card.Title>{curMenu.menuName}</Card.Title>
-                                <Card.Text>
-                                {curMenu.status}
-                                </Card.Text>
                             </Card.Body>
                             <ListGroup className="list-group-flush">
+                                <ListGroup.Item>{curMenu.menuStatus}</ListGroup.Item>
                                 {curMenu.timeRanges.map((time) => {
                                     var startStr = time.startTime.toString();
                                     var endStr = time.endTime.toString();
